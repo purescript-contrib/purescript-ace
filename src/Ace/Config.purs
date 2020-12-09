feb5e4cfb83db47dd899017d5a2f46d29d762732
Cyril <sobierajewicz.cyril@gmail.com>
2020-12-10 00:00:45 +0100

    Add roles declarations to forbid unsafe coercions (#32)

--- src/Ace/Config.purs
+++ src/Ace/Config.purs
@@ -16,6 +16,8 @@ import Data.Function.Uncurried (Fn2, runFn2)
 newtype ConfigOption :: Type -> Type
 newtype ConfigOption a = ConfigOption String
 
+type role ConfigOption representational
+
 foreign import setImpl :: forall a. Fn2 (ConfigOption a) a (Effect Ace)
 
 set :: forall a. ConfigOption a -> a -> Effect Ace
